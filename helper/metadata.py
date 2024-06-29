2f40e66d90be1b67694cda04b2b95c303c272728
Vegapunk-shaka <slac777f@tuta.io>
2024-06-29 16:49:33 +0600

    Update metadata.py

--- helper/metadata.py
+++ helper/metadata.py
@@ -5,8 +5,8 @@ from asyncio.subprocess import PIPE
 from bot import LOGGER
 
 async def change_metadata(file, dirpath, key):
-    LOGGER.info(f"Starting metadata modification for file: {file}")
-    temp_file = f"{file}.temp.mkv"
+    LOGGER.info(f"Starting metadata modification for file: {new_file_name}")
+    temp_file = f"{new_file_name}.temp.mkv"
     full_file_path = os.path.join(dirpath, file)
     temp_file_path = os.path.join(dirpath, temp_file)
 
@@ -90,44 +90,9 @@ async def change_metadata(file, dirpath, key):
     if process.returncode != 0:
         err = stderr.decode().strip()
         LOGGER.error(err)
-        LOGGER.error(f"Error modifying metadata for file: {file}")
+        LOGGER.error(f"Error modifying metadata for file: {new_file_name}")
         return file
 
     os.replace(temp_file_path, full_file_path)
-    LOGGER.info(f"Metadata modified successfully for file: {file}")
-    return file
-
-async def add_attachment(file, dirpath, attachment_path):
-    LOGGER.info(f"Adding photo attachment to file: {file}")
-
-    temp_file = f"{file}.temp.mkv"
-    full_file_path = os.path.join(dirpath, file)
-    temp_file_path = os.path.join(dirpath, temp_file)
-    
-    attachment_ext = attachment_path.split('.')[-1].lower()
-    if attachment_ext in ['jpg', 'jpeg']:
-        mime_type = 'image/jpeg'
-    elif attachment_ext == 'png':
-        mime_type = 'image/png'
-    else:
-        mime_type = 'application/octet-stream'
-
-    cmd = [
-        'ffmpeg', '-y', '-i', full_file_path,
-        '-attach', attachment_path,
-        '-metadata:s:t', f'mimetype={mime_type}',
-        '-c', 'copy', '-map', '0', temp_file_path
-    ]
-
-    process = await create_subprocess_exec(*cmd, stderr=PIPE, stdout=PIPE)
-    stdout, stderr = await process.communicate()
-
-    if process.returncode != 0:
-        err = stderr.decode().strip()
-        LOGGER.error(err)
-        LOGGER.error(f"Error adding photo attachment to file: {file}")
-        return file
-
-    os.replace(temp_file_path, full_file_path)
-    LOGGER.info(f"Photo attachment added successfully to file: {file}")
+    LOGGER.info(f"Metadata modified successfully for file: {new_file_name}")
     return file
